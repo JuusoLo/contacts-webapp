97fdc6ec74a1fb9b326254c76e64dd54b9d07e98
JuusoLo <lonka.juuso@gmail.com>
2018-05-16 12:09:15 +0300

    contacts app update, still in progress.

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -2,7 +2,7 @@ export class Contact {
   firstName: string;
   lastName: string;
 
-  constructor(firstName: string, lastName: string) {
+  constructor(id?: number, firstName?: string, lastName?: string) {
     this.firstName = firstName;
     this.lastName = lastName;
   }

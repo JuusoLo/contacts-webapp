a6545cbc20253871a55d754094f23a06e80c8321
JuusoLo <lonka.juuso@gmail.com>
2018-05-22 14:13:50 +0300

    Contacts update, editingEnabled etc...

--- src/app/contact/contact-detail/contact-detail.component.ts
+++ src/app/contact/contact-detail/contact-detail.component.ts
@@ -12,35 +12,40 @@ import {ToolbarAction} from '../../UI/toolbar-action';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
-
+  editingEnabled: boolean;
   contact: Contact;
 
   constructor(private router: Router, private route: ActivatedRoute,
               private contactService: ContactService, private toolbar: ToolbarService) {
     this.contact = new Contact();
+    this.editingEnabled = false;
   }
 
   ngOnInit() {
-    this.toolbar.toolbarOptions.next(
-      new ToolbarOptions(
-        'Create Contact', [
-          new ToolbarAction(this.onEdit, 'edit')
-        ]));
 
     const contactId = this.route.snapshot.paramMap.get('id');
+      let toolbarActions: ToolbarAction[];
 
     if (contactId == null) {
-      return;
+      // Create contact
+      this.editingEnabled = true;
+        toolbarActions = [];
+    } else {
+      // View/Edit contact
+      toolbarActions = [new ToolbarAction(this.onEdit.bind(this), 'edit')];
+      this.contactService.getContactById(contactId).subscribe(response => {
+        this.contact = response;
+        console.log(this.contact);
+      }, error => {
+        console.error('Getting contact failed!');
+        console.error(error);
+        this.router.navigate(['/contacts']);
+      });
     }
+    this.toolbar.toolbarOptions.next(
+      new ToolbarOptions('Contact', toolbarActions));
 
-    this.contactService.getContactById(contactId).subscribe(response => {
-      this.contact = response;
-      console.log(this.contact);
-    }, error => {
-      console.error('Getting contact failed!');
-      console.error(error);
-      this.router.navigate(['/contacts']);
-    });
+
   }
 
   onNavigateBack(): void {
@@ -53,5 +58,6 @@ export class ContactDetailComponent implements OnInit {
 
   onEdit() {
     console.log('TODO: activate/deactivate edit mode');
+    this.editingEnabled = !this.editingEnabled;
   }
 }
